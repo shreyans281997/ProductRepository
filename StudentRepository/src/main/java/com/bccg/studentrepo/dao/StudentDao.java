57d162f7182642ba6da4f76fa9e3bc037dba61e3
Shreyans <Shreyans@DESKTOP-4LMU8L5>
2019-07-30 02:46:57 +0530

    @component updated 2:46am 30-07-2019

--- StudentRepository/src/main/java/com/bccg/studentrepo/dao/StudentDao.java
+++ StudentRepository/src/main/java/com/bccg/studentrepo/dao/StudentDao.java
@@ -1,4 +1,6 @@
 package com.bccg.studentrepo.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import com.bccg.studentrepo.beans.Student;
+@Repository
 public interface StudentDao extends JpaRepository<Student, Integer> {}
--- StudentRepository/src/main/java/com/bccg/studentrepo/services/StudentServices.java
+++ StudentRepository/src/main/java/com/bccg/studentrepo/services/StudentServices.java
@@ -1,8 +1,10 @@
 package com.bccg.studentrepo.services;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import com.bccg.studentrepo.beans.Student;
 import com.bccg.studentrepo.dao.StudentDao;
+@Service
 public class StudentServices implements IStudentServices {
 	@Autowired
 	StudentDao daoServices;
